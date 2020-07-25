4ca0116e55ea599a721d2ff9257ea59c4877ca75
Daniel <nodoubt0322@gmail.com>
2020-07-25 18:20:26 +0800

    run build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "魔電中文網 (Median XL)",
-  description: "魔電中文相關資源網站",
+  description: "魔電中文(Median XL)相關資源網站",
   themeConfig: {
     nav: [{ text: "回首頁", link: "/" }],
     sidebar: ["/", "/install/", "/leveling/", "/merc/", "/cube/", "/guide/", "/trade/", "/eq/", "/speed/", "/bn/", "/uber/", "/occultEffigies/", "/umo/", "/d2status/"],
