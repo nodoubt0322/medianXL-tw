f7bb0279e179cbec457f813ad1400d59a7a5f3e9
Daniel <nodoubt0322@gmail.com>
2020-07-25 23:06:58 +0800

    add ga and scroll to top plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,15 @@
 module.exports = {
   title: "魔電中文網 (Median XL)",
   description: "魔電中文(Median XL)相關資源網站",
+  plugins: [
+    "@vuepress/back-to-top",
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-173584490-1",
+      },
+    ],
+  ],
   themeConfig: {
     nav: [{ text: "回首頁", link: "/" }],
     sidebar: ["/", "/install/", "/leveling/", "/merc/", "/cube/", "/guide/", "/trade/", "/eq/", "/speed/", "/bn/", "/uber/", "/occultEffigies/", "/umo/", "/d2status/"],
